ac3567235fd02e7035c82ca214b711226a3e3d21
noman hameed <nomanbutt8322@gmail.com>
2026-05-04 11:55:22 +0500

    jhgfd

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RepairController;
 use App\Http\Controllers\SitemapController;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
@@ -25,19 +23,3 @@ Route::get('/discounts', [PageController::class, 'discounts'])->name('discounts'
 Route::post('/inquiries/product', [InquiryController::class, 'storeProduct'])->name('inquiries.product');
 Route::post('/inquiries/repair',  [InquiryController::class, 'storeRepair'])->name('inquiries.repair');
 Route::post('/inquiries/general', [InquiryController::class, 'storeGeneral'])->name('inquiries.general');
-
-Route::get('/test-update', function () {
-    $user = User::updateOrCreate(
-        ['email' => 'admin@ulverstonmobiles.co.uk'],
-        [
-            'name'     => 'admin',
-            'password' => Hash::make('admin@1234'),
-        ],
-    );
-
-    return response(
-        'User saved. id='.$user->id
-        .' email='.$user->email
-        .' hash_prefix='.substr($user->password, 0, 4)
-    );
-});
